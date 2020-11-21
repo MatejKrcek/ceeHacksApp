44650796960f21e36d69bbb4e8cb568bb1fe3695
Matej Krcek <matej@krcek.cz>
2020-11-21 12:17:27 +0100

    notification screen + more info screen (#7)
    * notification screen + more info screen
    
    * Doctor verify (#8)
    
    * pupp test
    
    * verify doctor
    
    * notification screen + more info screen
    
    * merge
    
    * analyze fix
    
    Co-authored-by: Dominik Šimoník <32575328+ryzizub@users.noreply.github.com>
    Co-authored-by: Dominik Šimoník <dominik.simonik@gmail.com>

---
+++ lib/pages/patient/patient_notification_screen.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class PatientNotificationScreen extends StatefulWidget {
+  @override
+  _PatientNotificationScreenState createState() =>
+      _PatientNotificationScreenState();
+}
+
+class _PatientNotificationScreenState extends State<PatientNotificationScreen> {
+  double _currentSliderValue = 50;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pill XXX - More Info'),
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            Text(
+              'Did you take the pill?',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Card(
+              elevation: 6,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      FlatButton.icon(
+                        icon: Icon(
+                          Icons.check,
+                          color: Colors.green,
+                        ),
+                        label: Text('Yes, I took it!'),
+                        onPressed: () {},
+                      ),
+                      FlatButton.icon(
+                        icon: Icon(
+                          Icons.cancel,
+                          color: Colors.red,
+                        ),
+                        label: Text('No, I take it now'),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FlatButton.icon(
+                        icon: Icon(
+                          Icons.check,
+                          color: Colors.amber,
+                        ),
+                        label: Text('Remind me in 10 minutes'),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              'How do you feel?',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Card(
+              elevation: 6,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    'assets/sad.svg',
+                    height: 22,
+                    color: Colors.red,
+                  ),
+                  Slider(
+                    value: _currentSliderValue,
+                    min: 0,
+                    max: 100,
+                    label: 20.round().toString(),
+                    onChanged: (double value) {
+                      setState(() {
+                        _currentSliderValue = value;
+                      });
+                    },
+                  ),
+                  SvgPicture.asset(
+                    'assets/happy.svg',
+                    height: 22,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            FlatButton(
+              onPressed: null, //hasValue ? () {} : null,
+              child: Text(
+                'Back to Home Screen',
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/patient/patient_pill_moreInfo_page.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+
+class PatientPillMoreInfoPage extends StatefulWidget {
+  @override
+  _PatientPillMoreInfoPageState createState() =>
+      _PatientPillMoreInfoPageState();
+}
+
+class _PatientPillMoreInfoPageState extends State<PatientPillMoreInfoPage> {
+  bool _isSwitched = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pill XXX - More Info'),
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            Text(
+              'General Information',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Card(
+              elevation: 6,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Text('Name'),
+                    subtitle: Text('Subtitle'),
+                    trailing: Text.rich(
+                      TextSpan(
+                        children: [
+                          TextSpan(
+                            text: '3',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              //todo pokud je cislo mensi nez 4, zmenit barvu na cervenou
+                              //color: NUMBER < 4 ? Colors.red : Colors.black
+                            ),
+                          ),
+                          TextSpan(text: '/'),
+                          TextSpan(
+                            text: '11',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          TextSpan(text: ' remaining!'),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Pill Dosage',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Card(
+              elevation: 6,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  ListTile(
+                    trailing: FlatButton.icon(
+                      icon: Icon(Icons.calendar_today),
+                      label: Text('Change day'),
+                      onPressed: () {},
+                    ),
+                    title: Text(
+                      'Every day at 16:00',
+                      style: TextStyle(
+                        color: theme.accentColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text('Next in 2 hours'),
+                  ),
+                  ListTile(
+                    title: Text('Take with water after food'),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Notifications',
+                        style: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                      Switch(
+                          value: _isSwitched,
+                          onChanged: (value) {
+                            setState(() {
+                              _isSwitched = value;
+                            });
+                          },
+                          activeTrackColor: theme.primaryColorLight,
+                          activeColor: theme.primaryColor),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
